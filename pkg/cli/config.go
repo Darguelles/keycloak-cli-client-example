e5858a34773c62964d89d875edb7ff19fe70b5bd
Diego Rojas <diego.arguellesrojas@teampicnic.com>
2024-01-18 18:07:48 +0100

    Add PKCE support

--- pkg/cli/config.go
+++ pkg/cli/config.go
@@ -1,21 +1,27 @@
 package cli
 
-import "fmt"
+import (
+	"fmt"
+	"github.com/nirasan/go-oauth-pkce-code-verifier"
+)
 
 type Config struct {
-	KeycloakConfig KeycloakConfig
+	KeycloakConfig       KeycloakConfig
 	EmbeddedServerConfig EmbeddedServerConfig
 }
 
 type KeycloakConfig struct {
 	KeycloakURL string
-	Realm string
-	ClientID string
+	Realm       string
+	ClientID    string
 }
 
 type EmbeddedServerConfig struct {
-	Port         uint32
-	CallbackPath string
+	Port                uint32
+	CallbackPath        string
+	CodeChallengeMethod string
+	CodeChallenge       string
+	CodeVerifier        *go_oauth_pkce_code_verifier.CodeVerifier
 }
 
 func (c *EmbeddedServerConfig) GetCallbackURL() string {
--- pkg/cli/urls.go
+++ pkg/cli/urls.go
@@ -8,17 +8,22 @@ import (
 )
 
 func BuildAuthorizationRequest(config Config) string {
-	return fmt.Sprintf("%v/realms/%v/protocol/openid-connect/auth?client_id=%v&redirect_uri=%v&response_type=code", config.KeycloakConfig.KeycloakURL, config.KeycloakConfig.Realm, config.KeycloakConfig.ClientID, config.EmbeddedServerConfig.GetCallbackURL())
+	return fmt.Sprintf("%v/realms/%v/protocol/openid-connect/auth?client_id=%v"+
+		"&redirect_uri=%v&response_type=code&code_challenge_method=%v&code_challenge=%v",
+		config.KeycloakConfig.KeycloakURL, config.KeycloakConfig.Realm, config.KeycloakConfig.ClientID,
+		config.EmbeddedServerConfig.GetCallbackURL(), config.EmbeddedServerConfig.CodeChallengeMethod,
+		config.EmbeddedServerConfig.CodeChallenge)
 }
 
-func BuildTokenExchangeRequest(config Config, code string) (*http.Request, error) {
+func BuildTokenExchangeRequest(config Config, code string, codeVerifier string) (*http.Request, error) {
 	tokenURL := fmt.Sprintf("%v/realms/%v/protocol/openid-connect/token", config.KeycloakConfig.KeycloakURL, config.KeycloakConfig.Realm)
 
 	body := url.Values{
-		"grant_type":   {"authorization_code"},
-		"code":         {code},
-		"client_id":    {config.KeycloakConfig.ClientID},
-		"redirect_uri": {config.EmbeddedServerConfig.GetCallbackURL()},
+		"grant_type":    {"authorization_code"},
+		"code":          {code},
+		"client_id":     {config.KeycloakConfig.ClientID},
+		"redirect_uri":  {config.EmbeddedServerConfig.GetCallbackURL()},
+		"code_verifier": {codeVerifier},
 	}
 	req, err := http.NewRequest("POST", tokenURL, strings.NewReader(body.Encode()))
 	req.Header.Set("Content-Type", "application/x-www-form-urlencoded")
